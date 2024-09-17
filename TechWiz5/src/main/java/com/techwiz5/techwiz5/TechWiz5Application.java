0710ed166363eee3ba848d44a691ddf5934a19fb
Phuong <phuongphuong262204@gmail.com>
2024-09-17 22:38:35 +0700

    initial create

---
+++ TechWiz5/src/main/java/com/techwiz5/techwiz5/TechWiz5Application.java
@@ -0,0 +1,13 @@
+package com.techwiz5.techwiz5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechWiz5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechWiz5Application.class, args);
+    }
+
+}
